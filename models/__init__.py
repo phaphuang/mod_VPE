646841f1b99c9eaf972c0ded972831df4ca19468
phaphuang <aniwat.pha@gmail.com>
2021-03-31 14:47:41 +0700

    Add new vqvae model

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from models.vaeIdsiaStn import *
+from models.vqvae import VQVAEModel
 
 def get_model(name, n_classes=None):
     model = _get_model_instance(name)
@@ -10,6 +11,9 @@ def get_model(name, n_classes=None):
     if name is 'vaeIdsia':
         model = model(nc=3, input_size = 64, latent_variable_size=300, cnn_chn=[100, 150, 250] ,param1=None, param2=None, param3 = None) # idsianet cnn_chn=[100,150,250] latent = 300
         print('Use vae+Idsianet (without stns) with random initialization!')
+    
+    if name is 'vqvae':
+        model = model(num_hiddens=128, num_residual_layers=2, num_residual_hiddens=32, num_embeddings=512, embedding_dim=64, commitment_cost=0.25, decay=0.99)
 
     return model
 
@@ -18,6 +22,7 @@ def _get_model_instance(name):
         return {
             'vaeIdsiaStn' : VAEIdsia,
             'vaeIdsia' : VAEIdsia,
+            'vqvae' : VQVAEModel,
         }[name]
     except:
         print('Model {} not available'.format(name))
